718a9b853a9e47442e2aa173db2e57b6728e9edd
Jonathan Vingiano <jvingiano@gmail.com>
2010-10-18 17:58:16 -0400

    no comma after last artist

--- inc/blog.php
+++ inc/blog.php
@@ -2,14 +2,12 @@
 				<div class="section">
 				<h1 class="section-title">Blog</h1>
 					<p>Here's a blog blog blog blog blog blog blog blogblog blog blog blogblog blog blog blogblog blog blog blogblog blog blog blogblog blog blog blogblog blog blog blogblog blog blog blogblog blog blog blog</p>
-					<ul>
+					<ul id="blog-list">
 						<li>Tags</li>
 						<li class="blog-ul-separator">/</li>
 						<li><a href="<?php bloginfo('rss2_url'); ?>">RSS</a></li>
 						<li class="blog-ul-separator">/</li>
 						<li><a href="mailto:<?php the_author_meta('user_email',1); ?>">email</a></li>
-						<li class="blog-ul-separator">/</li>
-						<li>Tips</li>
 					</ul>
 				</div>
 			</div>
