3e1e9677264e8c39a1a938bfec4627de7411f2c5
Jonathan Vingiano <jvingiano@gmail.com>
2010-10-19 12:47:53 -0400

    got related posts working, added credits section

--- index.php
+++ index.php
@@ -1,121 +1,146 @@
 <?php 
-	define('WP_USE_THEMES', false); 
-	get_header();
-	get_sidebar(); 	
+define('WP_USE_THEMES', false); 
+get_header();
+get_sidebar(); 	
 ?>
 
 <hr style="visibility:hidden" id="free">
-
-<?php query_posts('category_name=free');
-         while (have_posts()) : the_post();  ?>
-            <div class="section-wrap">
-               <div class="section">
-               <?php the_title('<h1 class="section-title">', '</h1>'); ?>
-               <?php the_content(); ?>
-               </div>
-            </div>
-         <?php endwhile;
-      rewind_posts();
+  
+  <?php query_posts('category_name=free');
+  while (have_posts()) : the_post();  ?>
+  <div class="section-wrap">
+    <div class="section">
+      <?php the_title('<h1 class="section-title">', '</h1>'); ?>
+      <?php the_content(); ?>
+    </div>
+  </div>
+  <?php endwhile;
+                           rewind_posts();
       
       include('inc/artists.php');
-
-      query_posts('category_name=artists'); ?>
-        <?php while (have_posts()) : the_post();  ?>
-            <div class="section-wrap" id="artists-container">
-               <div class="section-artists" id="<?php echo strtolower(str_replace(' ','', trim(get_the_title($post))));?>">
-                  <div class="meta-wrapper">
-                  <?php the_post_thumbnail(array(646,auto)); ?>
-                  <div class="meta">
-                     <h2 class="ital"><?php echo get_post_meta($post->ID, 'artwork_title', true)?></h2>
-                     <?php the_title('<h3>', '</h3>'); ?>
-                  </div>
-                  </div>
-               <div class="artists-content">
-                  <?php the_content(); ?>
-                  <?php/* $title = strtolower(get_the_title()); ?>
-                  <?php $query = "category_name=blog&tag={$title}"; ?>
-                  <?php $artists_query = query_posts($query); ?>
-                  <?php while ($artists_query->have_posts()) : $artists_query->the_post();  ?>                 
-                  <?php the_title();?>
-                  <?php endwhile; ?>
-                  <?php /* foreach ($posts as $related){
-                    echo $related;
-                    echo $related->name;
-                    print_r($related);
+      
+  query_posts('category_name=artists'); ?>
+  <?php while (have_posts()) : the_post();  ?>
+  <div class="section-wrap" id="artists-container">
+    <div class="section-artists" id="<?php echo strtolower(str_replace(' ','', trim(get_the_title($post))));?>">
+    <div class="meta-wrapper">
+      <?php the_post_thumbnail(array(646,auto)); ?>
+      <div class="meta">
+        <h2 class="ital"><?php echo get_post_meta($post->ID, 'artwork_title', true)?></h2>
+        <?php the_title('<h3>', '</h3>'); ?>
+      </div>
+    </div>
+    <div class="artists-content">
+      <?php the_content(); ?>
+      
+      <?php
+        //for use in the loop, list 5 post titles related to first tag on current post
+                    $tags = /*strtolower(get_the_title());*/wp_get_post_tags($post->ID);
+                    if ($tags) {
+                      echo '<h3 class="red">External Links</h3>';
+                      $first_tag = $tags[0]->term_id;
+                      $args=array(
+                                  'tag__in' => array($first_tag),
+                                  'post__not_in' => array($post->ID),
+                                  'showposts'=>5,
+                                  'caller_get_posts'=>1
+                                  );
+                      $my_query = new WP_Query($args);
+                      if( $my_query->have_posts() ) {
+      while ($my_query->have_posts()) : $my_query->the_post(); ?>
+      <p><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
+      <?php
+        endwhile;
+  }
                     }
-                  */?>
+      ?>
+      
+    </div>
+  </div>
+</div>
+<?php endwhile; ?>
+<?php rewind_posts();
+
+include('inc/essays.php');
+
+query_posts('category_name=essays');
+while (have_posts()) : the_post();  ?>
+<div class="section-wrap essays" id="<?php the_author_meta(last_name); ?>">
+<div class="section">
+  <div class="essay-meta">
+    <?php the_title('<h2 class="ital">', '</h2>' ); ?>
+    <h3><?php the_author(); ?></h3>
+  </div>
+  <div class="essay-content">
+    <?php the_content(); ?>
+    <a href="#essay" class="closer">Close</a>
                </div>
-               </div>
-            </div>
+</div>
+</div>
          <?php endwhile; ?>
-     <?php rewind_posts();
-
-	include('inc/essays.php');
-
-      query_posts('category_name=essays');
+         <?php rewind_posts(); 
+                         
+                         include('inc/events.php');
+                         
+                         query_posts('category_name=events');
          while (have_posts()) : the_post();  ?>
-            <div class="section-wrap essays" id="<?php the_author_meta(last_name); ?>">
-               <div class="section">
-               <div class="essay-meta">
-               <?php the_title('<h2 class="ital">', '</h2>' ); ?>
-               <h3><?php the_author(); ?></h3>
-               </div>
-               <div class="essay-content">
-               <?php the_content(); ?>
-               <a href="#essay" class="closer">Close</a>
-               </div>
-               </div>
-            </div>
-         <?php endwhile; ?>
-      <?php rewind_posts(); 
-
-	  include('inc/events.php');
-
-      query_posts('category_name=events');
-         while (have_posts()) : the_post();  ?>
-            <div class="section-wrap events">
-               <div class="section events" id ="<?php echo strtolower(str_replace(' ','', trim(get_the_title($post))));?>">
-               <?php the_title('<h2 class="ital">', '</h2>'); ?>
-               <h5>
-               <?php echo get_post_meta( $post->ID, 'event_date', true ); ?>
-               &ndash;
-               <?php echo get_post_meta( $post->ID, 'event_time', true ); ?>
-               &ndash;
-               <?php echo get_post_meta( $post->ID, 'event_location', true ); ?>
-               </h5>               
-               <?php the_content(); ?>
-               </div>
-            </div>
-         <?php endwhile;
-      rewind_posts();
-
-	include('inc/blog.php');
-
-      query_posts('category_name=blog');
-         while (have_posts()) : the_post();  ?>
-            <div class="section-wrap">
-               <div class="section blog">
-               <?php the_title('<h2 class="ital">', '</h2>'); ?>
-               <?php the_post_thumbnail(); ?>
-               <?php the_content(); ?>
-               <?php echo bloginfo('url') .'/#'. strtolower(str_replace(' ','', trim(get_the_title($post)))); /* the permalink */ ?> 
-               <h3 id="tags">Tags</h3>
-               <ul id="tag-list">
-               <?php
-               $last_tag = end(get_tags());
-               foreach(get_tags() as $tag){     
-                 if ($tag == $last_tag) { 
-               echo '<li>' . $tag->name . '</li>';
-               } else {
-               echo '<li>' . $tag->name . ',</li>';
-               }
-	    }
-	    unset($tag);
-	   ?> 
+         <div class="section-wrap events">
+           <div class="section events" id ="<?php echo strtolower(str_replace(' ','', trim(get_the_title($post))));?>">
+           <?php the_title('<h2 class="ital">', '</h2>'); ?>
+           <h5>
+             <?php echo get_post_meta( $post->ID, 'event_date', true ); ?>
+             &ndash;
+             <?php echo get_post_meta( $post->ID, 'event_time', true ); ?>
+             &ndash;
+             <?php echo get_post_meta( $post->ID, 'event_location', true ); ?>
+           </h5>               
+           <?php the_content(); ?>
+         </div>
+       </div>
+       <?php endwhile;
+         rewind_posts();
+         
+         
+       query_posts('category_name=credits');
+       while (have_posts()) : the_post();  ?>
+       <div class="section-wrap">
+         <div class="section">
+           <?php the_title('<h1 class="section-title">', '</h1>'); ?>
+           <?php the_content(); ?>
+         </div>
+       </div>
+       <?php endwhile;
+         rewind_posts();
+                                  
+                                  
+	 include('inc/blog.php');
+                                  
+         _posts('category_name=blog');
+       while (have_posts()) : the_post();  ?>
+       <div class="section-wrap">
+         <div class="section blog">
+           <?php the_title('<h2 class="ital">', '</h2>'); ?>
+           <?php the_post_thumbnail(); ?>
+           <?php the_content(); ?>
+           <?php echo bloginfo('url') .'/#'. strtolower(str_replace(' ','', trim(get_the_title($post)))); /* the permalink */ ?> 
+           <h3 id="tags">Tags</h3>
+           
+           <ul id="tag-list">
+             <?php
+                                $last_tag = end(get_tags());
+                                foreach(get_tags() as $tag){     
+                                  if ($tag == $last_tag) { 
+                                    echo '<li>' . $tag->name . '</li>';
+                                  } else {
+                                    echo '<li>' . $tag->name . ',</li>';
+                                  }
+                                }
+	       unset($tag);
+	     ?> 
                </ul>
-               </div>
-            </div>
-         <?php endwhile;
-
-
-get_footer(); ?>
+         </div>
+       </div>
+       <?php endwhile;
+         
+           
+           get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -4,9 +4,10 @@
       <li><a href="#artists">Artists</a></li>
       <li><a href="#essays">Essays</a></li>
       <li><a href="#events">Events</a></li>
+      <li><a href="#credits">Credits</a></li>
       <li><a href="#blog">Blog</a></li>
    </ul>
-   <p>oct. 19 &ndash; jan. 8<br />new museum<br /></p>
+   <p>oct. 19 &ndash; jan. 8<br />at the new museum<br /></p>
    <ul id="social">
    <li><?php include('inc/facebook.html'); ?></li>
    <li><?php include('inc/twitter.html'); ?></li>
